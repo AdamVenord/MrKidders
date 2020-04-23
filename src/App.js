32d9a8d183600b7080625f6aeb2ee7c882307d00
Adam Venord <AdamVenord7@gmail.com>
2020-04-23 15:14:31 -0600

    first little bit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Segment, Container, Image } from 'semantic-ui-react'
+import './App.css';
+import welcome from './Imgs/welcome.png'
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Segment inverted color="green">
+          <Image src={welcome} size="large" centered />
+        </Segment>
+      </Container>
+    </>
+  );
+}
+
+export default App;
