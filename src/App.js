412ffb06f49ef00bbf62b15050a4c5a47120a7a6
Adam Venord <AdamVenord7@gmail.com>
2020-05-01 17:30:23 -0600

    first ghost choice to stay in complete

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React, { Component } from 'react';
 import { Segment, Container, Image, Header, Button, Transition } from 'semantic-ui-react'
 import './App.css';
 import welcome from './Imgs/welcome.png'
+// import scrollToComponent from 'react-scroll-to-component'
 
 export default class App extends Component {
-  // change the visible names after you figure it out
   state = { 
-    visiblePiratesButton: false, 
-    // visible2: false,
-    // visible3: false,
     visibleGhostButton: false,
+    visiblePiratesButton: false, 
+    visibleChoiceToStayInButton: false,
+    // visible3: false,
   }
 
 
+  toggleVisibilityForGhostButton = () => 
+    this.setState((prevState) => ({ visibleGhostButton: !prevState.visibleGhostButton }))
+  
   toggleVisibilityForPiratesButton = () => 
     this.setState((prevState) => ({ visiblePiratesButton: !prevState.visiblePiratesButton }))
 
-  toggleVisibilityForGhostButton = () => 
-    this.setState((prevState) => ({ visibleGhostButton: !prevState.visibleGhostButton }))
+  toggleVisibilityForTheChoiceToStayIn = () => 
+    this.setState((prevState) => ({ visibleChoiceToStayInButton: !prevState.visibleChoiceToStayInButton }))
+
 
   render() {
-    const { visiblePiratesButton, visibleGhostButton } = this.state
+    const { visiblePiratesButton, visibleGhostButton, visibleChoiceToStayInButton } = this.state
 
     return (
       <>
+      <br />
+      <br />
+      <br />
+      <br />
         <Container centered>
           <Segment inverted color="purple">
             <Image src={welcome} size="large" centered />
@@ -32,40 +40,63 @@ export default class App extends Component {
             What Path Should We Choose?
           </Header>
           <Button 
+            inverted
+            color="black"
+            content={visibleGhostButton ? 'Hide' : 'Ghost' }
+            onClick={this.toggleVisibilityForGhostButton}
+          />
+          <Button 
             inverted 
             color="red"
-            content={visiblePiratesButton ? 'Show' : 'Hide' }
+            content={visiblePiratesButton ? 'Hide' : 'Show' }
             onClick={this.toggleVisibilityForPiratesButton}
           />
           <Button inverted>Other</Button>
           <Button inverted>Other</Button>
-          <Button 
-            inverted
-            color="black"
-            content={visibleGhostButton ? 'Show' : 'Hide' }
-            onClick={this.toggleVisibilityForGhostButton}
-          />
-          {/* pirates storyline */}
-            <Transition visible={visiblePiratesButton} animation='drop' duration={500}>
-              <Segment>
-                Begin Pirates Life
-              </Segment>
-            </Transition>
-          {/* end of pirates storyline */}
-
           {/* ghost storyline */}
             <Transition visible={visibleGhostButton} animation='drop' duration={500}>
               <Segment>
-                Begin Ghost Story
+                <p>This Story begins on a dark cloudy night. We're both wondering what
+                  we should do with the night
+                </p>
+                <Button 
+                  inverted 
+                  color="blue"
+                  content={visibleChoiceToStayInButton ? 'woot' : 'Stay In' }                  
+                  onClick={this.toggleVisibilityForTheChoiceToStayIn} 
+                />
+                <Button>
+                  Go Out
+                </Button>
               </Segment>
             </Transition>
-          {/* end of ghost storyline */}
+              {/* Choice to stay in story line */}
+                <Transition visible={visibleChoiceToStayInButton} animation='drop' duration={500}>
+                  <Segment>
+                    Oh Fun, In that case let's tell scary stories
+                  </Segment>
+                </Transition>
+              {/* Choice to stay in story line ending */}
+
+              {/* end of ghost storyline */}
+
+              {/* pirates storyline */}
+                <Transition visible={visiblePiratesButton} animation='drop' duration={500}>
+                  <Segment>
+                    Begin Pirates Life
+                  </Segment>
+                </Transition>
+              {/* end of pirates storyline */}
+
           
-          {/* put in the transition for the next part here, also when we start each story we 
-          are going to try and make each story line be in their corresponding button,
-          figure out more on this later, but it will be a great way to keep track of where
-          each story line is in the code */}
+          {/* start each story we are going to try and make each story line be in their 
+          corresponding button, figure out more on this later, but it will be a great 
+          way to keep track of where each story line is in the code */}
         </Container>
+      <br />
+      <br />
+      <br />
+      <br />
       </>
     );
   }
